42416cade08a48cd9a096573a759eaa10afa9168
Joss Manger <jossferatu@gmail.com>
2021-12-04 11:09:35 -0500

    removes c package

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Q_rsqrt_swift",
-            dependencies: ["Q_rsqrt_c"]),
+            dependencies: ["Q_rsqrt_C"]),
         .testTarget(
             name: "Qr-sqrt-swiftTests",
             dependencies: ["Q_rsqrt_swift"]),
